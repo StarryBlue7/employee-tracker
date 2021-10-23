4af770012b087ec43a773accb470f102ca120ad3
Vince <starryblue7@gmail.com>
2021-10-23 14:03:37 -0700

    add department and role queries

--- queries.js
+++ queries.js
@@ -16,4 +16,51 @@ function mainMenu() {
         });
 }
 
+function queryDepartment() {
+    const questions = [
+        {
+            message: 'Enter department name: ',
+            name: 'department'
+        }
+    ];
+
+    inquirer
+        .prompt(questions).then(answer => {
+            return answer.choice;
+        });
+}
+
+function queryRole(departmentsObj) {
+    const departments =  [];
+    departmentsObj.forEach(obj => {
+        const department = obj.name[0].toUpperCase() + obj.name.substring(1);
+        departments.push(department);
+    });
+
+    const questions = [
+        {
+            message: 'Enter role name: ',
+            name: 'role'
+        },
+        {
+            message: 'Enter role salary: ',
+            name: 'salary'
+        },
+        {
+            type: 'list',
+            message: 'Select role\'s department',
+            choices: departments, 
+                filter(val) {
+                    return val.toLowerCase();
+                },
+            name: 'department'
+        }
+    ];
+
+    inquirer
+        .prompt(questions).then(answer => {
+            return answer.choice;
+        });
+}
+
 module.exports = { mainMenu }
