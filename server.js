ed62644c9dc87b0e73b4f54f99d444069103ebaf
Vince <starryblue7@gmail.com>
2021-10-24 16:19:14 -0700

    format view data tables

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-const util = require('util');
 const mysql = require('mysql2');
-const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 const { mainMenu, queryAddDepartment, queryAddEmployee } = require('./queries')
 
@@ -14,9 +12,10 @@ const db = mysql.createConnection(
     console.log('Connected to the employee_db database.')
 );
 
-function view(table) {
-    db.query(`SELECT * FROM ${table}`, (err, results) => {
+function view(table, selection, join) {
+    db.query(`SELECT ${selection || '*'} FROM ${table} ${join || ''}`, (err, results) => {
         err ? console.error(err) : console.table(results);
+        main();
     });
 }
 
@@ -27,7 +26,7 @@ function getRoles() {
 }
 
 function getManagers() {
-    db.query(`SELECT first_name, last_name FROM employee`, (err, results) => {
+    db.query(`SELECT id, CONCAT_WS(' ', first_name, last_name) AS name FROM employee`, (err, results) => {
         err ? console.error(err) : console.log(results);
     });
 }
@@ -38,6 +37,7 @@ function addEmployee(employee) {
         VALUES ("${employee.first_name}", "${employee.last_name}", ${employee.role_id}, ${employee.manager_id})`, 
         (err, results) => {
             err ? console.error(err) : console.log(`Added ${employee.first_name} ${employee.last_name} to employee database.`);
+            main();
     });
 }
 
@@ -45,13 +45,22 @@ function main() {
     mainMenu().then(answer => {
         switch (answer.choice) {
             case 'View All Employees':
-                view('employee');
+                view(
+                    'employee', 
+                    'employee.id AS ID, employee.first_name AS "First Name", employee.last_name AS "Last Name", title AS Title, name AS Department, salary AS Salary, CONCAT_WS(\' \', manager.first_name, manager.last_name) AS Manager', 
+                    'LEFT JOIN role ON employee.role_id = role.id ' + 
+                    'LEFT JOIN department ON role.department_id = department.id ' +
+                    'LEFT JOIN employee AS manager ON employee.manager_id = manager.id');
                 break;
             case 'View All Roles':
-                view('role');
+                view(
+                    'role', 
+                    'title AS Role, salary AS Salary, name AS Department', 
+                    'LEFT JOIN department ON role.department_id = department.id '
+                );
                 break;
             case 'View All Departments':
-                view('department');
+                view('department', 'name AS Departments');
                 break;
             case 'Add Employee':
 
@@ -71,4 +80,9 @@ function main() {
     }).catch(err => console.error(err));
 }
 
-main();
+function init() {
+    // ascii();
+    main();
+}
+
+init ();
